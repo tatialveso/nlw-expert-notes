5b9daf15525d2b1d6566c1d78eb7bcd2fca3f18b
tatialveso <tatiialveso@gmail.com>
2024-02-07 15:32:00 -0300

    :sparkles: setting props & interface

--- src/app.tsx
+++ src/app.tsx
@@ -20,7 +20,7 @@ function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
+        <NoteCard note={} />
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,11 +1,18 @@
-function NoteCard() {
+interface NoteCardProps {
+    note: {
+        date: Date
+        content: string
+    }
+}
+
+function NoteCard({ note }: NoteCardProps) {
     return (
-        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <button className="rounded-md text-left bg-slate-800 p-5 flex flex-col gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-400">
-                há 4 dias
+                { note.date.toISOString() }
             </span>
-            <p className="text-sm leading-6 text-slate-300">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet praesentium quam fugiat corrupti sunt nam quisquam non aspernatur sapiente pariatur rerum assumenda quaerat rem maiores enim quo soluta, iure aperiam?
+            <p className="text-sm leading-6 text-slate-400">
+                { note.content }
             </p>
             <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </button>
