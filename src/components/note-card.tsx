67af701f711e6b7e4da8fe53d36ccdb2ff56c263
tatialveso <tatiialveso@gmail.com>
2024-02-07 09:59:51 -0300

    :lipstick: styling cards

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,14 @@
 function NoteCard() {
     return (
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative pointer-events-none">
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-400">
                 há 4 dias
             </span>
             <p className="text-sm leading-6 text-slate-300">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet praesentium quam fugiat corrupti sunt nam quisquam non aspernatur sapiente pariatur rerum assumenda quaerat rem maiores enim quo soluta, iure aperiam?
             </p>
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-        </div>
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </button>
     )
 }
 
