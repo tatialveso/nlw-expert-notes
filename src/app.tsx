b48a949346f7c8878fc3963a14bc18f31aabc674
tatialveso <tatiialveso@gmail.com>
2024-02-07 09:22:56 -0300

    :sparkles: adding application header

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,20 @@
+import logo from './assets/logo.svg'
+
 function App() {
   return (
-    <h1>
-      hello world
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={ logo } alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+    </div>
   )
 }
 
