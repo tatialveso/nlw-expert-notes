85ff2f0036bed5366d3cc6de9f741d82dab216a3
tatialveso <tatiialveso@gmail.com>
2024-02-07 16:44:37 -0300

    :lipstick: styling add new note hover

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -4,7 +4,7 @@ import { X } from 'lucide-react'
 function NewNoteCard() {
     return (
         <Dialog.Root>
-            <Dialog.Trigger className="rounded-md flex flex-col text-left bg-slate-700 p-5 gap-3">
+            <Dialog.Trigger className="rounded-md flex flex-col text-left bg-slate-700 p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
                 <span className="text-sm font-medium text-slate-400">
                     Adicionar nota
                 </span>
